4234d4523b815c8c10277fcb2d3890dbe7bad982
Akhil Chandewar <akhil.chandewar00@gmail.com>
2026-01-02 11:58:27 +0530

    auth button

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,18 @@
+"use server";
 
-function AuthLayout({ children }: { children: React.ReactNode }) {
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+
+
+async function AuthLayout({ children }: { children: React.ReactNode }) {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if(session){
+        return redirect('/');        
+    }
     return (
         <div>
             {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { getCurrentUser } from "@/modules/auth/actions";
+import UserButton from "@/modules/auth/components/UserButtton";
+
+export default async function Home() {
+  const user = await getCurrentUser();
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <UserButton user={user} />
+    </div>
   );
 }
