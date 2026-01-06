9380b63a295f17bd5488f7981b36e8ccc5094c8f
Akhil Chandewar <akhil.chandewar00@gmail.com>
2026-01-06 16:58:04 +0530

    auth handler url fix

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const {signIn , signUp , signOut , useSession , } = createAuthClient({
-    baseURL: "http://localhost:3000"
+    // baseURL: "http://localhost:3000"
+    baseURL: "https://convo-ai-xu2r-k7ua7k4q3-akhileshchandewars-projects.vercel.app"
 })
